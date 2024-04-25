f7501fc5a1dcb67ee7726fb40513df79ff63c3b8
nitinsahu-sahu <polydabra12@gmail.com>
2024-04-25 12:49:41 +0530

    Update>> Routing

---
+++ src/components/home/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section>
+      <div className="img-accordion">
+        <div className="img img1">
+          <p className='bgi1'></p>
+        </div>
+        <div className="img img2">
+          <p className='bgi2'>Img 2</p>
+        </div>
+
+        <div className="img img3">
+          <p className='bgi3'>Img 3</p>
+        </div>
+
+        <div className="img img4">
+          <p className='bgi4'>Img 4</p>
+        </div>
+
+        <div className="img img5">
+          <p className='bgi5'>Img 5</p>
+        </div>
+      </div>
+    </section>
+
+  )
+}
+
+export default Home;
