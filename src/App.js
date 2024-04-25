094eb688bcaba5a7a3b591b25cc674ee0c952f2d
nitinsahu-sahu <polydabra12@gmail.com>
2024-04-25 12:06:30 +0530

    Setup>> project setup

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="img-accordion">
+        <div className="img img1">
+          <p className='bgi1'></p>
+        </div>
+        <div className="img img2">
+          <p className='bgi2'>Img 2</p>
+        </div>
+
+        <div className="img img3">
+          <p className='bgi3'>Img 3</p>
+        </div>
+
+        <div className="img img4">
+          <p className='bgi4'>Img 4</p>
+        </div>
+
+        <div className="img img5">
+          <p className='bgi5'>Img 5</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
