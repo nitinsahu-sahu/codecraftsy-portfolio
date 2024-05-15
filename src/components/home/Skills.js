22c353f62721bf0bf5ed45f74106ad47949e9be4
nitinsahu-sahu <polydabra12@gmail.com>
2024-05-15 12:11:03 +0530

    Update>> Skills card

--- src/components/home/Skills.js
+++ src/components/home/Skills.js
@@ -6,12 +6,20 @@ const Skills = () => {
       <h1 className="text-2xl font-bold mb-4">Image Grid</h1>
       <div className="flex justify-center items-center">
         <div className="flex justify-center space-x-4">
-          <img
-            src="skills/html.png"
-            alt="Image 1"
-            className="w-24 h-24 shadow-2xl shadow-blue-900 opacity-100"
-          />
-          <img
+          <div className="bg-darkgreen w-24 h-28 shadow-2xl shadow-skill-shadow opacity-100 rounded-lg hover:bg-themeyellow cursor-pointer">
+
+          </div>
+          <div className="bg-darkgreen w-24 h-28 shadow-2xl shadow-skill-shadow opacity-100 rounded-lg hover:bg-themeyellow cursor-pointer">
+
+          </div>
+          <div className="bg-darkgreen w-24 h-28 shadow-2xl shadow-skill-shadow opacity-100 rounded-lg hover:bg-themeyellow cursor-pointer">
+
+          </div>
+          <div className="bg-darkgreen w-24 h-28 shadow-2xl shadow-skill-shadow opacity-100 rounded-lg hover:bg-themeyellow cursor-pointer">
+
+          </div>
+
+          {/* <img
             src="skills/js.png"
             alt="Image 2"
             className="w-24 h-24 shadow-2xl shadow-blue-900 opacity-100"
@@ -25,28 +33,46 @@ const Skills = () => {
             src="skills/node.png"
             alt="Image 4"
             className="w-24 h-24 shadow-2xl shadow-blue-900"
-          />
+          /> */}
         </div>
       </div>
       <div className="flex justify-center items-center">
         <div className="flex justify-center mt-10 space-x-4">
-          <img src="skills/php.png" alt="Image 1" className="w-24 h-24" />
-          <img src="skills/wp.png" alt="Image 2" className="w-24 h-24" />
-          <img src="skills/mysql.png" alt="Image 3" className="w-24 h-24" />
-          <img src="skills/larawel.png" alt="Image 4" className="w-24 h-24" />
+        <div className="bg-darkgreen w-24 h-28 shadow-2xl shadow-skill-shadow opacity-100 rounded-lg hover:bg-themeyellow cursor-pointer">
+
+</div>
+<div className="bg-darkgreen w-24 h-28 shadow-2xl shadow-skill-shadow opacity-100 rounded-lg hover:bg-themeyellow cursor-pointer">
+
+</div>
+<div className="bg-darkgreen w-24 h-28 shadow-2xl shadow-skill-shadow opacity-100 rounded-lg hover:bg-themeyellow cursor-pointer">
+
+</div>
+<div className="bg-darkgreen w-24 h-28 shadow-2xl shadow-skill-shadow opacity-100 rounded-lg hover:bg-themeyellow cursor-pointer">
+
+</div>
         </div>
       </div>
       <div className="flex justify-center items-center">
         <div className="flex justify-center mt-10 space-x-4">
-          <img src="skills/mongo.png" alt="Image 5" className="w-24 h-24" />
-          <img src="skills/git.png" alt="Image 6" className="w-24 h-24" />
-          <img src="skills/tailwind.png" alt="Image 7" className="w-24 h-24" />
+        <div className="bg-darkgreen w-24 h-28 shadow-2xl shadow-skill-shadow opacity-100 rounded-lg hover:bg-themeyellow cursor-pointer">
+
+</div>
+<div className="bg-darkgreen w-24 h-28 shadow-2xl shadow-skill-shadow opacity-100 rounded-lg hover:bg-themeyellow cursor-pointer">
+
+</div>
+<div className="bg-darkgreen w-24 h-28 shadow-2xl shadow-skill-shadow opacity-100 rounded-lg hover:bg-themeyellow cursor-pointer">
+
+</div>
         </div>
       </div>
       <div className="flex justify-center items-center">
         <div className="flex justify-center mt-10 space-x-4">
-          <img src="skills/bootstrap.png" alt="Image 8" className="w-24 h-24" />
-          <img src="skills/jquery.png" alt="Image 9" className="w-24 h-24" />
+        <div className="bg-darkgreen w-24 h-28 shadow-2xl shadow-skill-shadow opacity-100 rounded-lg hover:bg-themeyellow cursor-pointer">
+
+</div>
+<div className="bg-darkgreen w-24 h-28 shadow-2xl shadow-skill-shadow opacity-100 rounded-lg hover:bg-themeyellow cursor-pointer">
+
+</div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,13 @@ module.exports = {
       lightgreen: "#87A922",
       themeyellow: "#FCDC2A",
       skin: "#F7F6BB",
-      black:"#000000",
-      white:"#ffffff"
+      black: "#000000",
+      white: "#ffffff"
     },
     boxShadow: {
       'random-shadow': "rgba(100, 100, 111, 0.2) 1px 1px 9px 7px",
-      'contact-shadow': "2px 3px 14px black"
+      'contact-shadow': "2px 3px 14px black",
+      'skill-shadow': "6px 5px 12px 1px #471069"
     }
   },
   plugins: [
